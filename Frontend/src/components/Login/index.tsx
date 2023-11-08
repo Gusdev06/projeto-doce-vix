e929d8e0739c6f26f47942121d67199d596d2142
Gusdev <coven688@gmail.com>
2023-11-08 17:44:46 -0300

    finish login

--- Frontend/src/components/Login/index.tsx
+++ Frontend/src/components/Login/index.tsx
@@ -1,59 +1,71 @@
-import * as S from './styles'
-import PersonIcon from '@mui/icons-material/Person';
-import { TextField } from '@mui/material'
-import { FormEvent, useContext, useState } from "react"
-import { AuthContext } from '../../contexts/AuthContext'
+import PersonIcon from "@mui/icons-material/Person";
+import { TextField } from "@mui/material";
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import * as S from "./styles";
 
 const Login = () => {
-    const [modalIsOpen, setIsOpen] = useState(false)
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const { user } = useContext(AuthContext)
-    const { signIn } = useContext(AuthContext)
+  const { user, signIn, isAuthenticated } = useContext(AuthContext);
 
-    console.log(user)
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
 
-    async function handleSubmit(event: FormEvent) {
-        event.preventDefault();
-
-        const data = {
-            email,
-            password
-        }
-
-        await signIn(data)
-    }
-
-    const openModal = () => {
-        setIsOpen(true);
+    const data = {
+      email,
+      password,
     };
 
-    const closeModal = () => {
-        setIsOpen(false);
-    };
+    await signIn(data);
+  }
 
-    return (
-        <>
-            <S.divBotao>
-                <S.BotaoLoginHeader onClick={openModal}>
-                    <PersonIcon />
-                    Login
-                </S.BotaoLoginHeader>
-            </S.divBotao>
-            <S.ModalStyleLogin isOpen={modalIsOpen} onRequestClose={closeModal}>
-                <form onSubmit={handleSubmit}>
-                    <h1>Login</h1>
-                    <TextField value={email} onChange={e => setEmail(e.target.value)} name='email' type='email' id="outlined-basic" label="E-mail" variant="outlined" />
-                    <TextField value={password} onChange={e => setPassword(e.target.value)} name='password' type='password' id="outlined-basic" label="Senha" variant="outlined" />
-                    <S.BotaoLogar type='submit'>Logar</S.BotaoLogar>
-                    <p>Esqueceu a senha?</p>
-                    <p>Não possuí uma conta? <span>Criar Conta</span></p>
-                </form>
-            </S.ModalStyleLogin>
-            <h1>Conta - {user?.email}</h1>
-        </>
-    )
-}
+  const openModal = () => {
+    setIsOpen(true);
+  };
 
-export default Login
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <S.divBotao>
+        <S.BotaoLoginHeader onClick={openModal}>
+          <PersonIcon />
+          Login
+        </S.BotaoLoginHeader>
+      </S.divBotao>
+      <S.ModalStyleLogin isOpen={modalIsOpen} onRequestClose={closeModal}>
+        <form onSubmit={handleSubmit}>
+          <h1>Login</h1>
+          <TextField
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            type="email"
+            id="outlined-basic"
+            label="E-mail"
+            variant="outlined"
+          />
+          <TextField
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            type="password"
+            id="outlined-basic"
+            label="Senha"
+            variant="outlined"
+          />
+          <S.BotaoLogar type="submit">Logar</S.BotaoLogar>
+          <p>Esqueceu a senha?</p>
+          <h1> {user?.email}</h1>
+        </form>
+      </S.ModalStyleLogin>
+    </>
+  );
+};
+
+export default Login;
--- Frontend/src/contexts/AuthContext.tsx
+++ Frontend/src/contexts/AuthContext.tsx
@@ -1,76 +1,74 @@
-import { ReactNode, createContext, useEffect, useState } from "react"
-import { api } from "../services/api/api"
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
+import { ReactNode, createContext, useEffect, useState } from "react";
+import { api } from "../services/api/api";
 
 type User = {
-    email: string,
-    role: string,
-    userId: number
-}
+  userId: number;
+  email: string;
+  role: string;
+};
 
 type SignInCredentials = {
-    email: string
-    password: string
-}
+  email: string;
+  password: string;
+};
 
 type AuthContextData = {
-    signIn(credentials: SignInCredentials): Promise<void>
-    user: User | undefined
-    isAuthenticated: boolean
-}
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User | undefined;
+  isAuthenticated: boolean;
+};
 
 type AuthProviderProps = {
-    children: ReactNode
-}
+  children: ReactNode;
+};
 
-export const AuthContext = createContext({} as AuthContextData)
+export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-    const [user, setUser] = useState<User>()
-    const isAuthenticated = !!user
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
 
-    useEffect(() => {
-        const getToken = Cookies.get('token')
+  useEffect(() => {
+    const getToken = Cookies.get("token");
 
-        if (getToken) {
-            api.get('/').then(response => {
-                const { email, role, userId } = response.data
-                
-                setUser({ email, role, userId })
-            })
-        }
-    }, [])
+    if (getToken) {
+      const { userId, email, role } = JSON.parse(atob(getToken.split(".")[1]));
 
-    async function signIn({ email, password }: SignInCredentials) {
-        try {
-            const response = await api.post('login', {
-                email,
-                password
-            })
+      setUser({
+        userId,
+        email,
+        role,
+      });
 
-            const { role, userId, token } = response.data
-
-            Cookies.set('token', token, { expires: 30 })
-
-            setUser({
-                email,
-                role,
-                userId
-            })
-
-            api.defaults.headers['Authorization'] = `Bearer ${token}`
-
-            console.log(user)
-
-
-        } catch (err) {
-            console.log(err);
-        }
+      console.log(userId);
     }
+  }, []);
 
-    return (
-        <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
-            {children}
-        </AuthContext.Provider>
-    )
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post("login", {
+        email,
+        password,
+      });
+
+      const { role, userId, token } = response.data;
+
+      Cookies.set("token", token, { expires: 20 });
+
+      setUser({
+        userId,
+        email,
+        role,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
